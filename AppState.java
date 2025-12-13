9f76ca08c70f722e23bebadb54afee34fc3343ea
pabloo.snchzzz <146384662+Pabloosnchzzz@users.noreply.github.com>
2025-12-13 21:27:03 +0000

    Creacion de la clase AppState

---
+++ AppState.java
@@ -0,0 +1,11 @@
+package Entidades;
+
+public class AppState {
+
+    // Auth manager global
+    public static AuthManager authManager = new AuthManager();
+
+    // Usuario actualmente logeado
+    public static User usuarioActual = null;
+
+}
