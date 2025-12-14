eba35f45feae9d426c9f609f2b95d0499a2e1b8f
Pablo Martín Perdomo <147206381+Pancrasi0@users.noreply.github.com>
2025-12-14 13:05:44 +0000

    Fix package declaration in DiscountTicket.java

--- Entities/DiscountTicket.java
+++ Entities/DiscountTicket.java
@@ -1,4 +1,4 @@
-package Entidades;
+package Entities;
 
 public class DiscountTicket {
 
@@ -43,4 +43,5 @@ public class DiscountTicket {
         return company + " - " + description + " (" + requiredPoints + " pts)";
     }
 }
+
 
